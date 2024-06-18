00525fe668c4db2cc3cdf36f8d81d042c9d9090e
Miles Punch <miles.anyprintz@gmail.com>
2024-06-18 21:43:06 +1000

    bug fixes
    > be me
    > be extremely high
    > push code without testing it
    > ohwell.gif
    > find a really stupid naming convention
    > fix it and push 30 minutes after my last bug
    > rip another cone and go to bed
    > "i'm a software developer"

--- examples/Dimmers/Ranged Dimmer.ino
+++ examples/Dimmers/Ranged Dimmer.ino
@@ -1,5 +1,5 @@
 #include <OpenFixtures.h>   // Import my library
-RangedDimmerDimmer dimmer1(1, 6, 0, 255, 0, 511);   // Address 1 and pin 6, take the normal DMX Input, but only range the output from 0 to 511 out of 1023.
+RangedDimmer dimmer1(1, 6, 0, 255, 0, 511);   // Address 1 and pin 6, take the normal DMX Input, but only range the output from 0 to 511 out of 1023.
 
 void setup() {
 DMXSerial.init(DMXReceiver);   // This bit is kinda magic and if you ignore it, everything works fine!
